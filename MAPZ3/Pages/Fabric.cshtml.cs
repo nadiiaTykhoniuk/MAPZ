b64678a7f6711ba6f6dbc74b8ac0c2a74391092e
Nadia Tishoniuk <matsonka02@gmail.com>
2021-05-16 22:29:49 +0300

    fabrics and singleton

---
+++ MAPZ3/Pages/Fabric.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAPZ3.Pages
+{
+    abstract class AbstractFabricModel
+    {
+        public abstract WorkerUser MakeWorker();
+
+        public abstract AdminUser MakeAdmin();
+
+        public abstract ProjectManagerUser MakePM();
+    }
+    public class WorkerUser
+    {
+        public string Name()
+        {
+            return "I am worker";
+        }
+    }
+    public class AdminUser
+    {
+        public string Name()
+        {
+            return "I am admin";
+        }
+    }
+    public class ProjectManagerUser
+    {
+        public string Name()
+        {
+            return "I am PM";
+        }
+    }
+
+    class
+        MyOwnJira : AbstractFabricModel
+    {
+        public override WorkerUser MakeWorker()
+        {
+            return new WorkerUser();
+        }
+        public override AdminUser MakeAdmin()
+        {
+            return new AdminUser();
+        }
+
+        public override ProjectManagerUser MakePM()
+        {
+            return new ProjectManagerUser();
+        }
+    }
+
+}
---
+++ MAPZ3/Pages/Index.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAPZ3.Pages
+{
+    public class SingletonProgram : PageModel
+    {
+        private static readonly SingletonProgram instance = new SingletonProgram();
+        public static string text = "Singleton Program Created";
+        public string Date { get; private set; }
+
+        private readonly ILogger<SingletonProgram> _logger;
+
+        public SingletonProgram(ILogger<SingletonProgram> logger)
+        {
+            _logger = logger;
+            Date = System.DateTime.Now.TimeOfDay.ToString();
+        }
+
+        private SingletonProgram()
+        { 
+            
+        }
+
+        public static SingletonProgram GetInstance()
+        {
+            return instance;
+        }
+
+        public static string getWorker()
+        {
+            var jira = new MyOwnJira();
+            return jira.MakeWorker().Name();
+        }
+
+        public static string getAdmin()
+        {
+            var jira = new MyOwnJira();
+            return jira.MakeAdmin().Name();
+        }
+
+        public static string getProjectManager()
+        {
+            var jira = new MyOwnJira();
+            return jira.MakePM().Name();
+        }
+
+        public void OnGet()
+        {
+            ViewData["singleton"] = SingletonProgram.text;
+            ViewData["timecreated"] = System.DateTime.Now.TimeOfDay.ToString();
+            ViewData["workerUser"] = SingletonProgram.getWorker();
+            ViewData["workerAdmin"] = SingletonProgram.getAdmin();
+            ViewData["workerPM"] = SingletonProgram.getProjectManager();
+        }
+    }
+}
